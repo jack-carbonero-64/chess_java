57068e1859979d2c2088d84cba5ef95b3b062dbe
Jack CARBONERO <117528398+jack-carbonero-64@users.noreply.github.com>
2022-11-17 17:25:33 +0100

    Board - Number of rows and columns - Set as static and add their getters

--- src/main/java/chess/graphics/Board.java
+++ src/main/java/chess/graphics/Board.java
@@ -12,8 +12,8 @@ import chess.pieces.Piece.Team;
 public class Board extends JPanel 
 {
     // Number of rows and columns on the board
-    private final int BOARD_ROW_NUMBER = 8;
-    private final int BOARD_COLUMN_NUMBER = 8;
+    private static final int BOARD_ROW_NUMBER = 8;
+    private static final int BOARD_COLUMN_NUMBER = 8;
 
     /**
      * Constructor that creates the board
@@ -25,40 +25,40 @@ public class Board extends JPanel
         // Board settings
         this.setName("Board panel");
         this.setBackground(Color.GREEN);
-        this.setLayout(new GridLayout(this.BOARD_ROW_NUMBER, this.BOARD_COLUMN_NUMBER));
+        this.setLayout(new GridLayout(BOARD_ROW_NUMBER, BOARD_COLUMN_NUMBER));
 
         // Create squares and place pieces on the board
-        for (int y = 0; y < this.BOARD_ROW_NUMBER; y++)
+        for (int y = 0; y < BOARD_ROW_NUMBER; y++)
         {
             // Set team side
             Team team = null;
 
             if (y < 2) 
                 team = Team.BLACK;
-            else if (y >= (this.BOARD_ROW_NUMBER - 2))
+            else if (y >= (BOARD_ROW_NUMBER - 2))
                 team = Team.WHITE;
 
-            for (int x = 0; x < this.BOARD_COLUMN_NUMBER; x++)
+            for (int x = 0; x < BOARD_COLUMN_NUMBER; x++)
             {
                 Point pos = new Point(x, y);
 
                 // Pawns line
-                if (y == 1 || y == (this.BOARD_ROW_NUMBER - 2))
+                if (y == 1 || y == (BOARD_ROW_NUMBER - 2))
                     this.add(new Square(pos, new Pawn(pos, team)));
 
                 // Other pieces line
-                else if (y == 0 || y == (this.BOARD_ROW_NUMBER - 1))
+                else if (y == 0 || y == (BOARD_ROW_NUMBER - 1))
                 {
                     // Rooks
-                    if (x == 0 || x == (this.BOARD_COLUMN_NUMBER - 1))
+                    if (x == 0 || x == (BOARD_COLUMN_NUMBER - 1))
                         this.add(new Square(pos, new Rook(pos, team)));
                     
                     // Knights
-                    else if (x == 1 || x == (this.BOARD_COLUMN_NUMBER - 2))
+                    else if (x == 1 || x == (BOARD_COLUMN_NUMBER - 2))
                         this.add(new Square(pos, new Knight(pos, team)));
                     
                     // Bishops
-                    else if (x == 2 || x == (this.BOARD_COLUMN_NUMBER - 3))
+                    else if (x == 2 || x == (BOARD_COLUMN_NUMBER - 3))
                         this.add(new Square(pos, new Bishop(pos, team)));
                     
                     // Queens
@@ -75,5 +75,25 @@ public class Board extends JPanel
                     this.add(new Square(pos));
             }
         }
+    }
+
+    /**
+     * Getter for the number of rows in the board
+     *
+     * @return Number of rows in the board
+     */
+    public static int getBoardRowNumber()
+    {
+        return BOARD_ROW_NUMBER;
+    }
+
+    /**
+     * Getter for the number of columns in the board
+     *
+     * @return Number of columns in the board
+     */
+    public static int getBoardColumnNumber()
+    {
+        return BOARD_COLUMN_NUMBER;
     }
 }
